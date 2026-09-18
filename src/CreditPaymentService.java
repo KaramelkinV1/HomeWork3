737b9d7cadbfaff5adecd5f2e42960a7330f564b
Dezmond <karelin.dimentr@gmail.com>
2026-09-18 13:12:42 +0800

    Format code

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,9 +1,9 @@
 public class CreditPaymentService {
-    public int calculate (int amount, double rate, int months) {
+    public int calculate(int amount, double rate, int months) {
         double monthlyRate;
         monthlyRate = rate / 100 / 12;
         double coefficient;
-        coefficient = Math.pow( 1 + monthlyRate, months );
+        coefficient = Math.pow(1 + monthlyRate, months);
         double payment;
         payment = (amount * monthlyRate * coefficient) / (coefficient - 1);
         return (int) payment;
--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,10 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        CreditPaymentService service = new  CreditPaymentService();
-        int payment1 = service.calculate(1_000_000,9.99, 12);
-        int payment2 = service.calculate(1_000_000,9.99, 24);
-        int payment3 = service.calculate(1_000_000,9.99, 36);
+        CreditPaymentService service = new CreditPaymentService();
+        int payment1 = service.calculate(1_000_000, 9.99, 12);
+        int payment2 = service.calculate(1_000_000, 9.99, 24);
+        int payment3 = service.calculate(1_000_000, 9.99, 36);
         System.out.println(payment1);
         System.out.println(payment2);
         System.out.println(payment3);
